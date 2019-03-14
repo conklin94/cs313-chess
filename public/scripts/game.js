889b4fa4402aca1739e0a7693edfa63a88e2a0b7
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-03-14 17:12:27 -0600

    Added pieces

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,46 +1,45 @@
 function getImage(piece) {
-  alert("stuff may have happened");
   var image = "";
   switch(piece) {
   case 'r':
-    image = "https://upload.wikimedia.org/wikipedia/commons/a/a0/Chess_rdt60.png";
+    image = "images/black_rook.png";
     break;
   case 'n':
-    image = "https://upload.wikimedia.org/wikipedia/commons/f/f1/Chess_ndt60.png";
+    image = "images/black_knight.png";
     break;
   case 'b':
-    image = "https://upload.wikimedia.org/wikipedia/commons/8/81/Chess_bdt60.png";
+    image = "images/black_bishop.png";
     break;
   case 'k':
-    image = "https://upload.wikimedia.org/wikipedia/commons/e/e3/Chess_kdt60.png";
+    image = "images/black_king.png";
     break;
   case 'q':
-    image = "https://upload.wikimedia.org/wikipedia/commons/a/af/Chess_qdt60.png";
+    image = "images/black_queen.png";
     break;
   case 'p':
-    image = "https://upload.wikimedia.org/wikipedia/commons/c/cd/Chess_pdt60.png";
+    image = "images/black_pawn.png";
     break;
   case 'R':
-    image = "https://upload.wikimedia.org/wikipedia/commons/5/5c/Chess_rlt60.png";
+    image = "images/white_rook.png";
     break;
   case 'N':
-    image = "https://upload.wikimedia.org/wikipedia/commons/2/28/Chess_nlt60.png";
+    image = "images/white_knight.png";
     break;
   case 'B':
-    image = "https://upload.wikimedia.org/wikipedia/commons/9/9b/Chess_blt60.png";
+    image = "images/white_bishop.png";
     break;
   case 'K':
-    image = "https://upload.wikimedia.org/wikipedia/commons/3/3b/Chess_klt60.png";
+    image = "images/white_king.png";
     break;
   case 'Q':
-    image = "https://upload.wikimedia.org/wikipedia/commons/4/49/Chess_qlt60.png";
+    image = "images/white_queen.png";
     break;
   case 'P':
-    image = "https://upload.wikimedia.org/wikipedia/commons/0/04/Chess_plt60.png";
+    image = "images/white_pawn.png";
     break;
   default:
     return image;
   }
-  var newHTML = "<img class='piece' scr='" + image + "' alt=''>";
+  var newHTML = "<img class='piece' src='" + image + "' alt='" + piece + "'>";
   return newHTML;
 }
