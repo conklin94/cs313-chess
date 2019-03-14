ce3aa09823413340b856f11e8e744b1c0daf9463
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-03-14 17:30:30 -0600

    server

--- server.js
+++ server.js
@@ -53,16 +53,3 @@ function getGame (req, res) {
 function createGame (req, res) {
   console.log("game_code: " + req.body.game_code);
 }
-/*app.get("/getPerson", function (req, res){
-    const id = req.query.id;
-    pool.query('SELECT * FROM person WHERE person_id = $1', [id], (err, result) => {
-        if (err) {
-          res.status(500).json({success: false, data: error});
-        }
-        else {
-          const person = result.rows[0];
-          console.log(person);
-			    res.status(200).json(person);
-        }
-    });
-});*/
