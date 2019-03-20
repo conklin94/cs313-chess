4ef9a7fafa372cdce09437da00479628cbf9062d
Seth Conklin <sethconklin@205-185-99-45.directcom.com>
2019-03-19 21:53:40 -0600

    Ability to create new game, reset board, save board, and get current board

--- public/scripts/game.js
+++ public/scripts/game.js
@@ -1,6 +1,3 @@
-var captured_by_black = "";
-var captured_by_white = "";
-
 function getImage(piece) {
   var image = "";
   switch(piece) {
@@ -56,6 +53,34 @@ function makeBoard(board, captured_by_white, captured_by_black) {
   var cell = 0;
   var row = 0;
   var piece = 0;
+  var white_jail = document.getElementById('captured_by_white');
+  var black_jail = document.getElementById('captured_by_black');
+  white_jail.innerHTML = "";
+  black_jail.innerHTML = "";
+  for (var i = 0; i < captured_by_white.length; i++) {
+    var letter = captured_by_white.charAt(i);
+    var image = getImage(letter);
+    var imageHTML = `<img id="piece${piece}" src='${image}' `
+                  + `alt='${letter}' ondragstart="drag(event)"`
+                  + ` draggable="true">`;
+    white_jail.innerHTML += `<td class="captured" ondrop="drop(event)"`
+                         + ` ondragover="allowDrop(event)">`
+                         + `${imageHTML}</td>`;
+    piece += 1;
+
+  }
+  for (var i = 0; i < captured_by_black.length; i++) {
+    var letter = captured_by_black.charAt(i);
+    var image = getImage(letter);
+    var imageHTML = `<img id="piece${piece}" src='${image}' `
+                  + `alt='${letter}' ondragstart="drag(event)"`
+                  + ` draggable="true">`;
+    black_jail.innerHTML += `<td class="captured" ondrop="drop(event)"`
+                         + ` ondragover="allowDrop(event)">`
+                         + `${imageHTML}</td>`;
+    piece += 1;
+  }
+
   chess_board.innerHTML = "";
   board_string = `<tr id="row${row}">`;
   row += 1;
@@ -97,6 +122,16 @@ function drop(ev) {
   ev.preventDefault();
   var data = ev.dataTransfer.getData("text");
   var captured = null;
+  var image = document.getElementById(data);
+  if (image.parentNode == ev.target || image == ev.target) {
+    return;
+  }
+  // Delete the td tag if the piece is brought back onto the board
+  if (image.parentNode.parentNode.id == "captured_by_black"
+      || image.parentNode.parentNode.id == "captured_by_white") {
+    image.parentNode.parentNode.removeChild(image.parentNode);
+  }
+  console.log();
   if (ev.target.hasChildNodes()) {
     var captured = ev.target.childNodes[0];
     ev.target.innerHTML = "";
@@ -105,10 +140,10 @@ function drop(ev) {
     var parentNode = ev.target.parentNode;
     var captured = ev.target;
     parentNode.innerHTML = "";
-    parentNode.appendChild(document.getElementById(data));
+    parentNode.appendChild(image);
   }
   else {
-    ev.target.appendChild(document.getElementById(data));
+    ev.target.appendChild(image);
   }
   handleCaptured(captured);
 }
@@ -117,9 +152,8 @@ function handleCaptured(captured) {
   captured_by_black = document.getElementById('captured_by_black');
   captured_by_white = document.getElementById('captured_by_white');
   if (captured === null) {
-    return;
+    return "";
   }
-  console.log(captured);
   if (captured.alt == captured.alt.toUpperCase()) {
     captured_by_black.innerHTML += `<td class="captured" ondrop="drop(event)"`
                                 + ` ondragover="allowDrop(event)">`
@@ -130,4 +164,113 @@ function handleCaptured(captured) {
                                 + ` ondragover="allowDrop(event)">`
                                 + `${captured.outerHTML}</td>`;
   }
+}
+
+function submitBoard(game_code) {
+  var captured_by_white = getCapturedByWhite();
+  var captured_by_black = getCapturedByBlack();
+  var board = getBoardEncoding();
+  postAjax('/board', `game_code=${game_code}&board=${board}&captured_by_black=`
+                      + `${captured_by_black}&captured_by_white=`
+                      + `${captured_by_white}`,
+           function(data){ console.log(data); });
+}
+
+function resetBoard(game_code) {
+  var captured_by_white = '';
+  var captured_by_black = '';
+  var board = 'rnbqkbnr/pppppppp/00000000/00000000/00000000/00000000/PPPPPPPP/'
+            + 'RNBQKBNR';
+  postAjax('/board', `game_code=${game_code}&board=${board}&captured_by_black=`
+                      + `${captured_by_black}&captured_by_white=`
+                      + `${captured_by_white}`,
+           function(data){ console.log(data);
+             getCurrentBoard(game_code);
+           });
+
+}
+
+function getCurrentBoard(game_code) {
+  getAjax(`/board?game_code=${game_code}`, function(data){
+    var json = JSON.parse(data);
+    var board = json['board'];
+    var captured_by_white = json['captured_by_white'];
+    var captured_by_black = json['captured_by_black'];
+    makeBoard(board, captured_by_white, captured_by_black);
+  });
+}
+
+function getBoardEncoding() {
+  var board = "";
+  for (var i = 0; i < 64; i++) {
+    var currentID = `cell${i}`;
+    var currentCell = document.getElementById(currentID);
+    if (currentCell.hasChildNodes()) {
+      board += currentCell.firstChild.alt;
+    }
+    else {
+      board += "0"
+    }
+    if (i % 8 == 7 && i != 63) {
+      board += "/"
+    }
+  }
+  return board;
+}
+
+function getCapturedByBlack() {
+  var captured = "";
+  var element = document.getElementById('captured_by_black');
+  if (element.hasChildNodes()) {
+    var children = element.childElementCount;
+    for (var i = 0; i < children; i++) {
+      var currentCell = element.childNodes[i];
+      if (currentCell.hasChildNodes()) {
+        captured += currentCell.firstChild.alt;
+      }
+    }
+  }
+  return captured;
+}
+
+function getCapturedByWhite() {
+  var captured = "";
+  var element = document.getElementById('captured_by_white');
+  if (element.hasChildNodes()) {
+    var children = element.childElementCount;
+    for (var i = 0; i < children; i++) {
+      var currentCell = element.childNodes[i];
+      if (currentCell.hasChildNodes()) {
+        captured += currentCell.firstChild.alt;
+      }
+    }
+  }
+  return captured;
+}
+
+function postAjax(url, data, success) {
+    var params = typeof data == 'string' ? data : Object.keys(data).map(
+            function(k){ return encodeURIComponent(k) + '=' + encodeURIComponent(data[k]) }
+        ).join('&');
+
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject("Microsoft.XMLHTTP");
+    xhr.open('POST', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState>3 && xhr.status==200) { success(xhr.responseText); }
+    };
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+    xhr.send(params);
+    return xhr;
+}
+
+function getAjax(url, success) {
+    var xhr = window.XMLHttpRequest ? new XMLHttpRequest() : new ActiveXObject('Microsoft.XMLHTTP');
+    xhr.open('GET', url);
+    xhr.onreadystatechange = function() {
+        if (xhr.readyState>3 && xhr.status==200) success(xhr.responseText);
+    };
+    xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+    xhr.send();
+    return xhr;
 }
--- server.js
+++ server.js
@@ -25,6 +25,8 @@ app.get('/', (req, res) => res.render('pages/index'));
 
 app.get('/game', getGame);
 app.post('/game', createGame);
+app.get('/board', getBoard);
+app.post('/board', updateBoard);
 
 function getGame (req, res) {
   var game_code = req.query.game_code;
@@ -42,7 +44,7 @@ function getGame (req, res) {
           console.log(game_params);
         }
         else {
-          console.log("twas undefined");
+          console.log("Undefined game code");
           const params = {msg: "Error: the game code you entered does not exist"};
           res.render("pages/index", params);
         }
@@ -52,4 +54,53 @@ function getGame (req, res) {
 
 function createGame (req, res) {
   console.log("game_code: " + req.body.game_code);
+  var game_code = req.body.game_code;
+  var board = "rnbqkbnr/pppppppp/00000000/00000000/00000000/00000000/PPPPPPPP/"
+            + "RNBQKBNR";
+  pool.query('INSERT INTO game (game_code, board) VALUES ($1, $2)',
+             [game_code, board], (err, result) => {
+      if (err) {
+        console.log("This id already exists");
+        const params = {msg: "Error: game code already exists"};
+        res.render("pages/index", params);
+      }
+      else {
+        res.writeHead(302, { 'Location': `/game?game_code=${game_code}`});
+        res.end();
+      }
+  });
+}
+
+function updateBoard (req, res) {
+  var game_code = req.body.game_code;
+  var board = req.body.board;
+  var captured_by_white = req.body.captured_by_white;
+  var captured_by_black = req.body.captured_by_black;
+  console.log("New board: " + req.body.board);
+  pool.query('UPDATE game SET board = $1, captured_by_white = $2, captured_by_black = $3 WHERE game_code = $4',
+             [board, captured_by_white, captured_by_black, game_code], (err, result) => {
+      if (err) {
+        res.send("Unable to update board");
+      }
+      else {
+        res.send("Success");
+      }
+  });
+}
+
+function getBoard (req, res) {
+  var game_code = req.query.game_code;
+  console.log("game_code: " + req.query.game_code);
+  const id = req.query.id;
+  pool.query('SELECT game_id, game_code, board, captured_by_white, captured_by_black FROM game WHERE game_code = $1',
+             [game_code], (err, result) => {
+      if (err) {
+        res.send("Unable to get board");
+      }
+      else {
+        const game_params = result.rows[0];
+          res.send(game_params);
+      }
+  });
+
 }
